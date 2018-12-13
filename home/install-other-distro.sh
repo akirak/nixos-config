ef1dce21381b28731125c3a035327c5e83a93559
Akira Komamura <akira.komamura@gmail.com>
2018-12-13 13:41:55 +0000

    Replace ~/.config with ~/nixos/home on non-NixOS installation

--- home/install-other-distro.sh
+++ home/install-other-distro.sh
@@ -15,7 +15,20 @@ fi
 
 # If the configuration directory is not available, make the directory available
 if [ ! -e $HOME/.config/nixpkgs ]; then
-    ln -s $HOME/nixos/home/nixpkgs $HOME/.config/nixpkgs
+    # Move all files in ~/.config to ~/nixos/home and remove ~/.config
+    # This might not be safe, so use it with caution
+    if [ -d $HOME/.config ]; then
+        mv -v -t $HOME/nixos/home $HOME/.config/*.* $HOME/.config/*
+        rmdir $HOME/.config
+    fi
+    ln -s $HOME/nixos/home $HOME/.config
+
+    # This is safer, but not all config files are available in the target system
+    # ln -s $HOME/nixos/home/nixpkgs $HOME/.config/nixpkgs
+
+    # overlayfs is not available on Crostini on Chrome OS, so
+    # you can't use this:
+    #
     # workdir=$(mktemp -d hm-config-overlayfsXXX)
     # mount -t overlay overlay \
     #       -o lowerdir=$HOME/nixos/home,upperdir=$HOME/.config,workdir=$workdir \
